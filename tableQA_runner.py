2b69175869310ff5b6c8c984e746e36ed6df8343
SV <sv@SVs-MacBook-Air.local>
2017-03-29 23:49:03 +0200

    new: save model

--- tableQA_runner.py
+++ tableQA_runner.py
@@ -2,6 +2,8 @@ import glob
 import os
 import random
 import sys
+import gzip
+import pickle
 
 import argparse
 import numpy as np
@@ -15,14 +17,14 @@ random.seed(seed_val)
 np.random.seed(seed_val)  # for reproducing
 
 
-def run_tableQA(data_path):
+def run_tableQA(data_path, model_file):
     """
     Train and test for table QA
     """
 
     # Parse data
     train_files = glob.glob(data_path.format('train'))
-    test_files  = glob.glob(data_path.format('train'))
+    test_files  = glob.glob(data_path.format('test'))
 
     dictionary = {"nil": 0}
     train_story, train_questions, train_qstory = parse_babi_task(train_files, dictionary, False)
@@ -39,15 +41,22 @@ def run_tableQA(data_path):
 
     test(test_story, test_questions, test_qstory, memory, model, loss, general_config)
 
+    # save_model
+    with gzip.open(model_file, "wb") as f:
+        print("Saving model to file %s ..." % model_file)
+        reversed_dict = dict((ix, w) for w, ix in dictionary.items())
+        pickle.dump((reversed_dict, memory, model, loss, general_config), f)
+
 
 if __name__ == "__main__":
+    data_path = './data/test_data_{}.txt'
     # data_path = './data/synth_data_{}.txt'
     # data_path = './data/sim_data_{}.txt'
-    data_path = './data/table_data_{}.txt'
+    # data_path = './data/table_data_{}.txt'
 
     # Check if data is available
     # if not os.path.exists(data_dir):
     #     print("The data directory '%s' does not exist. Please download it first." % data_dir)
     #     sys.exit(1)
     
-    run_tableQA(data_path)
+    run_tableQA(data_path, './trained_model/memn2n_table_qa_model.pklz')
